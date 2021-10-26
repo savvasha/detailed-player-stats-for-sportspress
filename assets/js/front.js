a1ad63ce198e53a6e66eb28996cafd94cb8721a0
Savvas Hadjigeorgiou <savvasha@hotmail.com>
2021-10-26 11:19:28 +0300

    We need two different js functions. One for popup and one for inline mode

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-	$("button.player-season-stats").click(function() {
+	$("button.player-season-stats-popup").click(function() {
 
 		//Confirm that div#player_events is empty
 		$( '#player_events' ).empty();
@@ -28,6 +28,40 @@ jQuery(document).ready(function($) {
 			success : function( response ) {
 				$('#player_events').html( response );
 				tb_show(player_name + ' @ ' + competition_name, '#TB_inline?&width=640&height=300&inlineId=player_events', false);
+			},
+			error : function (response){
+			}
+		})
+	})
+	
+	$("button.player-season-stats-inline").click(function() {
+
+		var competition_name = $(this).data('competition_name');
+		var player_name = $(this).data('player_name');
+		var league_id = $(this).data('league_id');
+		var season_id = $(this).data('season_id');
+		var team_id = $(this).data('team_id');
+		var player_id = $(this).data('player_id');
+		var nonce = $(this).data('nonce');
+		
+		//Confirm that div#player_events is empty
+		$( '#player_events_'+league_id ).empty();
+		
+		//Call player_season_matches() function and return the response to div#player_events and from there to thickbox
+		var ajax_call = $.ajax({
+			url  : the_ajax_script.ajaxurl,
+			type : 'post',
+			data : {
+				action: 'player_season_matches',
+				competition_name: competition_name , 
+				league_id: league_id,
+				season_id: season_id,
+				team_id: team_id,
+				player_id: player_id,
+				nonce: nonce
+			},
+			success : function( response ) {
+				$( '#player_events_'+league_id ).html( response );
 			},
 			error : function (response){
 			}
